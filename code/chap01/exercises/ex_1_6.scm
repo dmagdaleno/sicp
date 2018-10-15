3cce75d096364fa4ff4d1d57ecb6abd2fc501415
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-10-14 22:58:51 -0300

    add chapter 1 exercise 6

---
+++ code/chap01/exercises/ex_1_6.scm
@@ -0,0 +1,11 @@
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+;Aborting!: maximum recursion depth exceeded
